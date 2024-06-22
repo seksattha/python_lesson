883e8353ecf42192f98e2c67690d489d5f8ec17d
seksatta_HP <seksattha@gmail.com>
2024-06-22 13:44:45 +0700

    version 0.2 : add into fucntion.

--- mysql/sql_connection.py
+++ mysql/sql_connection.py
@@ -1,26 +1,28 @@
 import mysql.connector
 
-try:
-    with mysql.connector.connect(
-        host= 'localhost',
-        user = 'root',
-        password = 'ditepd',
-        database = 'pcb'
-    )as con:
-        if con.is_connected():
-            print("connection to DIT_EPD successfully")
+def query_data(host:str, user:str, password, database_name):
+    try:
+        with mysql.connector.connect(
+            host= host,
+            user = user,
+            password = password,
+            database = database_name
+        )as con:
+            if con.is_connected():
+                print("connection to DIT_EPD successfully, \nWELCOME TO EPD DATABASE!")
 
-            with con.cursor() as cur:
-                sql_cmd =  """
-                select * from main;
-                """
-                cur.execute(sql_cmd)
+                with con.cursor() as cur:
+                    sql_cmd =  """
+                    select * from main where error_code = 'U4';
+                    """
+                    cur.execute(sql_cmd)
+                    rows = cur.fetchall()
 
-                rows = cur.fetchall()
+                    for row in rows:
+                        print(row)
 
-                for row in rows:
-                    print(row)
+    except Exception as e:
+        print(f'error => {e}')
 
-except Exception as e:
-    print(f'error = {e}')
-
+if __name__ == '__main__':
+    query_data('localhost', 'root', 'ditepd', 'pcb' )
