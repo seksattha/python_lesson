8649f96e655a2b615bc33914879df20461f6e667
seksatta_HP <seksattha@gmail.com>
2024-07-06 15:35:03 +0700

    add vars

--- OOP2/4.vars_getattr.py
+++ OOP2/4.vars_getattr.py
@@ -9,7 +9,10 @@ class Person:
         today = date.today()
         return today.year - self.dob.year
     def __str__(self):
-        return f'name: {self.fname} surname {self.lname} age:{self.age()} years'
+        # return f'name: {self.fname} surname {self.lname} age:{self.age()} years'
+        a = vars(self)
+        print(a)
+        return ""
 
 if __name__ == '__main__':
     p = Person('Ronnie', 'O Sullivan', date(1975, 12 ,5))
