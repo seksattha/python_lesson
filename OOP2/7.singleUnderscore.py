56918bbaa83f9458766bef99f05846ba12004dee
seksatta_HP <seksattha@gmail.com>
2024-07-06 16:13:17 +0700

    private variable

---
+++ OOP2/7.singleUnderscore.py
@@ -0,0 +1,10 @@
+# import numpy as np
+#
+# for _ in range(5):
+#     print("hello")
+#
+# for _ in range(100):
+#     print(np.random.randint(1,10))
+
+_recipe = "Pad Kapro" # private ห้ามไม่ให้คนอื่นไปยุ่ง หรือว่าไม่เปลี่ยนค้่า
+vat = 0.7
