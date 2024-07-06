357b323df0c368f4f7eeca69e19f6d0e429b07ec
seksatta_HP <seksattha@gmail.com>
2024-07-06 15:11:05 +0700

    add string method

--- OOP2/3.__str__ vs __repr.py
+++ OOP2/3.__str__ vs __repr.py
@@ -1,4 +1,4 @@
-class Medel:
+class Medal:
     def __init__(self, country, gold, silver, bronze):
         self.country = country
         self.silver = silver
@@ -9,13 +9,22 @@ class Medel:
         return self.gold + self.silver + self.bronze
 
     def __str__(self):
-        return f'country => {self.country:>3} gold: {self.gold} silver: {self.silver} bronze: {self.bronze}'
+        return f'country => {self.country:>3} gold: {self.gold:>3} silver: {self.silver:3} bronze: {self.bronze:3}'
 if __name__ == '__main__':
-    th = Medel("Thailand", 5, 6, 10 )
+    th = Medal("Thailand", 5, 6, 10 )
     print(th)
     #เขียนแบบนี้ จะไม่สดวกเวลาใช้งานจริง
     print(th.country, th.gold, th.silver ,th.bronze)
     print(th.total())
+
+    m = [
+        Medal("Japan", 1,2 ,3),
+        Medal('USA', 3, 7, 5),
+        
+    ]
+
+    for  c in m:
+        print(c)
 
 
     print(th)
