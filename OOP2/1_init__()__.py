e433af7f5b6ce11c3d2ebe62477eaee8508ba2ae
seksatta_HP <seksattha@gmail.com>
2024-07-06 14:00:43 +0700

    V 0.2

--- OOP2/1_init__()__.py
+++ OOP2/1_init__()__.py
@@ -4,6 +4,26 @@ class Person:
         self.lanme = ""
         self.country = "Thailand" #กำหนดให้ defult เป็นประเทศไทย
 
+class Person2:
+    def __init__(self, fname, lname, country):
+        self.fname = fname
+        self.lname = lname
+        self.country = country
+
+class Medal:
+    def __init__(self, country, gold, silver , bronze):
+        self.country = country
+        self.bronze = bronze
+        self.silver = silver
+        self.gold = gold
+
+
 if __name__ == '__main__':
-    p1 = Person()   # Create new instance
-    print(p1.country)
+    # p1 = Person()   # Create new instance
+    # print(p1.country)
+    #
+    # p1 = Person2()
+    # print(p1)
+
+    th = Medal('Thailand', 5, 3, 2)
+    print(th)
