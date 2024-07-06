459629d1c84dba3757979879effeda64505ed603
seksatta_HP <seksattha@gmail.com>
2024-07-06 14:19:14 +0700

    V 0.4 add bmi class

--- OOP2/1_init__()__.py
+++ OOP2/1_init__()__.py
@@ -23,7 +23,27 @@ class Medal:
     def dummy(self , a, b):
         return  a + b
 
+class Bmi:
+    def __init__(self, weight, height):
+        self.weight = weight
+        self.height = height
 
+    def bmi_calc(self): #instance method
+        return self.weight / (self.height/100) ** 2
+
+    def bmi_cat(self):
+        if self.bmi_calc() < 18.5:
+            text  = "ต่ำกว่าเกณฑ์"
+        elif 18.5 <= self.bmi_calc() <= 25:
+            text = "ตามเกณฑ์"
+        elif 25 <= self.bmi_calc() <= 30:
+            text = "เกินเกณฑ์"
+        elif self.bmi_calc() > 30:
+            text = "อ้วน"
+        return text
+
+
+    
 
 if __name__ == '__main__':
     # p1 = Person()   # Create new instance
@@ -42,3 +62,7 @@ if __name__ == '__main__':
     print(Medal.total(th))
 
     print(th.dummy(2,3))
+
+    a = Bmi(70, 174)
+    print(a.bmi_calc())
+    print(a.bmi_cat())
