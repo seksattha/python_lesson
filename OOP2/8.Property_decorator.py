cdea462339e7daa29591a0dfcec8be92397899c8
seksatta_HP <seksattha@gmail.com>
2024-07-06 16:59:36 +0700

    properties

--- OOP2/8.Property_decorator.py
+++ OOP2/8.Property_decorator.py
@@ -10,8 +10,24 @@ class Student:
     @property
     def full_name2(self):
         return f'{self.fname} {self.lname} '
+    @property
+    def join_yy(self):
+        return self.id[:2]
+
+    @property
+    def get_major(self):
+        major_code = {
+            '1': 'Engineer',
+            '2': 'Scientist',
+            '3': 'Architecture',
+            '4': 'Accountance'
+        }
+        return major_code.get(self.id[2], 'Unknown Major')
+
 
 if __name__ == '__main__':
-    s = Student("123456789","John", "Doe")
+    s = Student("5841234526","John", "Doe")
     print(s.full_name())
     print(s.full_name2)
+    print(s.join_yy)
+    print(s.get_major)
