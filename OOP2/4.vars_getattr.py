7d1f69ba1927f704a4411fe523c505e28dd4da3f
seksatta_HP <seksattha@gmail.com>
2024-07-06 15:32:51 +0700

    complete first pattern

---
+++ OOP2/4.vars_getattr.py
@@ -0,0 +1,16 @@
+from datetime import  date
+class Person:
+    def  __init__(self, fname, lname, dob):
+        self.dob = dob
+        self.lname = lname
+        self.fname = fname
+
+    def age(self): #instance methode
+        today = date.today()
+        return today.year - self.dob.year
+    def __str__(self):
+        return f'name: {self.fname} surname {self.lname} age:{self.age()} years'
+
+if __name__ == '__main__':
+    p = Person('Ronnie', 'O Sullivan', date(1975, 12 ,5))
+    print(p)
