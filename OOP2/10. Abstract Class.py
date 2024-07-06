568d3daac11fed66cd305a43b52d005e9889ab14
seksatta_HP <seksattha@gmail.com>
2024-07-06 18:05:44 +0700

    basic pattern V1.0

---
+++ OOP2/10. Abstract Class.py
@@ -0,0 +1,23 @@
+class Member:
+    def __init__(self, id, fname, lname):
+        self.id = id
+        self.fname = fname
+        self.lname = lname
+
+    def __str__(self):
+        return "ID:{} {} {}".format(self.id, self.fname, self.lname)
+
+class Gold(Member):
+    pass
+
+class Silver(Member):
+    pass
+
+if __name__ == '__main__':
+    m = Member("007", "James", "bond")
+    print(m)
+    g = Gold("123","John", "Doe")
+    print(g)
+
+
+
