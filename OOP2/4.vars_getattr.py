15bc60948f047a79fb4099ffa85ee22cc6f25008
seksatta_HP <seksattha@gmail.com>
2024-07-06 15:43:13 +0700

    add attrs

--- OOP2/4.vars_getattr.py
+++ OOP2/4.vars_getattr.py
@@ -10,9 +10,15 @@ class Person:
         return today.year - self.dob.year
     def __str__(self):
         # return f'name: {self.fname} surname {self.lname} age:{self.age()} years'
-        a = vars(self)
-        print(a)
-        return ""
+        # a = vars(self)
+        # print(a)
+        # print(f'printing value in dictionary')
+        # s = [f'key = {k:10} value = {v}' for k,v in a.items()]
+        # return "\n".join(s)
+        attrs = ('fname', 'lname', 'dob') # attributes
+        s  = [ f'{a} {getattr(self,a)}'for a in attrs]
+        return "\n".join(s)
+
 
 if __name__ == '__main__':
     p = Person('Ronnie', 'O Sullivan', date(1975, 12 ,5))
