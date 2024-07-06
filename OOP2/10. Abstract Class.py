b7c75fc43e2f50373cfa2ed0052589c60e2f28e0
seksatta_HP <seksattha@gmail.com>
2024-07-06 18:19:07 +0700

    add abstract method V1.3

--- OOP2/10. Abstract Class.py
+++ OOP2/10. Abstract Class.py
@@ -8,21 +8,27 @@ class Member(ABC):
     def __str__(self):
         return "ID:{} {} {}".format(self.id, self.fname, self.lname)
 
+    #พอเขียนถึงตรงนี้จะสร้าง instance จาก class member ไม่ได้อีกแล้ว
     @abstractmethod
     def discount(self):
         pass
 
 class Gold(Member):
-    pass
+    def discount(self):
+        return .1
 
 class Silver(Member):
-    pass
+    def discount(self):
+        return .05
 
 if __name__ == '__main__':
-    m = Member("007", "James", "bond")
-    print(m)
+    # m = Member("007", "James", "bond")
+    # print(m)
     g = Gold("123","John", "Doe")
     print(g)
+    print(g.discount())
+    s = Silver("456","Jane", "Smith")
+    print(s.discount())
 
 
 
