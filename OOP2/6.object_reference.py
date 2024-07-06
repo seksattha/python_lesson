4ddf7751a8bb57ce53b84ccc971117971626273a
seksatta_HP <seksattha@gmail.com>
2024-07-06 16:08:42 +0700

    v0.2

--- OOP2/6.object_reference.py
+++ OOP2/6.object_reference.py
@@ -16,3 +16,12 @@ if __name__ == '__main__':
     dad.earn(100)
     dad.spend(23)
     print(dad)
+
+    mom = dad # ใช้เงินจากกระเป๋าเดียวก
+    print(mom is dad)
+
+    print(f'moms wallet amont {mom}')
+    mom.spend(200)
+    print(dad)
+    #พอทำหนดให้เท่ากันแล้ว จะอยุ่ใน memory ตำแหน่งเดียวกันเลย
+    print(f'{id(mom)} {id(dad)}')
