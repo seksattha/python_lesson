af8ed1ede9578d0dc988364ababbd8204c7aff33
seksatta_HP <seksattha@gmail.com>
2024-07-12 10:03:33 +0700

    2 เพิ่ม textvariable

--- GUI/TKinter/weight_calcualtion.py
+++ GUI/TKinter/weight_calcualtion.py
@@ -1,7 +1,10 @@
 from tkinter import  *
 
 root = Tk()
-
-ent_kg = Entry(root).pack(side=LEFT)
+tv_kg = DoubleVar
+ent_kg = Entry(root, textvariable=tv_kg).pack(side=LEFT)
 Label(root, text= "kg." ).pack(side= LEFT)
+
+Button(root, text= " Calculate").pack(side = LEFT)
+lbl_lbs = Label(root).pack(side= LEFT)
 root.mainloop()
