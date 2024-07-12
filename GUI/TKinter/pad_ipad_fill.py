3e5a4dcfbfbd956ea78e91ff2b88606fae3fadee
seksatta_HP <seksattha@gmail.com>
2024-07-12 09:52:47 +0700

    4 เพิ่ม pad

--- GUI/TKinter/pad_ipad_fill.py
+++ GUI/TKinter/pad_ipad_fill.py
@@ -1,11 +1,16 @@
 from tkinter import *
 
 # fill , padx, pady
+#ipad
+    # ipadx จะเป็นการกำหนดขนาดในแกน y
+    # ipadx จะเป็นการกำหนดขนาดในแกน x
 
-# ipadx จะเป็นการกำหนดขนาดในแกน y
-# ipadx จะเป็นการกำหนดขนาดในแกน x
+
+#pad
+    #padx จะเป็นกำหนดระยะห่างในแต่ละ widget ในแกน x
+    #pady จะเป็นกำหนดระยะห่างในแต่ละ widget ในแกน y
 root = Tk()
-Label(root, text="Mocha", bg = "red").pack(side= TOP, ipady = 20, ipadx  = 20)
+Label(root, text="Mocha", bg = "red").pack(side= TOP, ipady = 20, ipadx  = 20 , pady = 20)
 Label(root, text="Espresso", bg = "green").pack(side= TOP)
 Label(root, text="Latte", bg = "blue").pack(side= TOP)
 root.mainloop()
