20346d8c4aa1ba209307404d19db1d01e7e85aea
seksatta_HP <seksattha@gmail.com>
2024-07-12 09:50:21 +0700

    3 เพิ่ม ipadx, y

--- GUI/TKinter/pad_ipad_fill.py
+++ GUI/TKinter/pad_ipad_fill.py
@@ -2,8 +2,10 @@ from tkinter import *
 
 # fill , padx, pady
 
+# ipadx จะเป็นการกำหนดขนาดในแกน y
+# ipadx จะเป็นการกำหนดขนาดในแกน x
 root = Tk()
-Label(root, text="Mocha", bg = "red").pack(side= TOP)
+Label(root, text="Mocha", bg = "red").pack(side= TOP, ipady = 20, ipadx  = 20)
 Label(root, text="Espresso", bg = "green").pack(side= TOP)
 Label(root, text="Latte", bg = "blue").pack(side= TOP)
 root.mainloop()
