401edcc96c888cce3d36ba943bc0a99b941e8d9d
seksatta_HP <seksattha@gmail.com>
2024-07-12 09:09:59 +0700

    1 basic form

---
+++ GUI/TKinter/overview.py
@@ -0,0 +1,6 @@
+import tkinter as tk # import tkinter แล้วทำการตั้งชื่อว่า tk
+
+root = tk.Tk()             #คำว่า root จะเป้นอะไรก็ได้ main windows หรืออะไรก็ได้
+root.mainloop()
+
+# พื้นฐานก็เขียนเท่านี้ก็พอ
