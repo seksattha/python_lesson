2f22e66cdb8afdcca97bd4a747165bca1be862bc
seksatta_HP <seksattha@gmail.com>
2024-07-12 10:26:19 +0700

    3 กำหนด text variable

--- GUI/TKinter/weight_calcualtion.py
+++ GUI/TKinter/weight_calcualtion.py
@@ -1,10 +1,14 @@
 from tkinter import  *
 
 root = Tk()
-tv_kg = DoubleVar
+#สร้าง textvariable
+tv_kg = DoubleVar() #tv ย่อมาจาก Text variable
+tv_lbs = DoubleVar()
+
+
 ent_kg = Entry(root, textvariable=tv_kg).pack(side=LEFT)
 Label(root, text= "kg." ).pack(side= LEFT)
 
 Button(root, text= " Calculate").pack(side = LEFT)
-lbl_lbs = Label(root).pack(side= LEFT)
+lbl_lbs = Label(root, textvariable=tv_lbs).pack(side= LEFT)
 root.mainloop()
