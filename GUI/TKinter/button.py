fd8c7a824c6f2070b85428d5bc4cdc75d242e744
seksatta_HP <seksattha@gmail.com>
2024-07-12 09:30:44 +0700

    2. เพิ่มการตั้งต่า สีสรรต่างๆ เข้าไป ทั้ง foreground และ backgroun

--- GUI/TKinter/button.py
+++ GUI/TKinter/button.py
@@ -1,7 +1,11 @@
 from tkinter import *
 
 root = Tk()
+root.option_add("*Font", "consolars 20")
 Button(root, text="Open").pack() # เป็นการสร้างปุ่ม
 # การกำหนดให้เป็น pack ที่จะเป็นการกำหนดให้แสดงผลจาก บนลงล่าง
+Button(root, text="Click me ", bg= "orange").pack()
+# จะเป็นการสร้าง ปุ่ม ที่มี พื้นหลังเป็นสีส้ม โดยการตั้งค่าจาก bg
 
+Button(root, text = "Click me", bg='Black', fg=  'yellow').pack()
 root.mainloop()
