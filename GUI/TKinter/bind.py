6f258b6e8959d6bbb3839ed631655dbd0c6f42d7
seksatta_HP <seksattha@gmail.com>
2024-07-12 10:45:58 +0700

    2

--- GUI/TKinter/bind.py
+++ GUI/TKinter/bind.py
@@ -3,13 +3,19 @@ from tkinter import *
 root = Tk()
 
 
+#ทำการสร้าง textvariable
+tv_male = IntVar()
+tv_female = IntVar()
+tv_total  = IntVar()
+
 #กำหนดปุ่มมาสองอัน
 Button(root, text='male').grid(row=0, column = 0)
 Button(root, text='female').grid(row=0, column = 1)
 
-#ทำการสร้าง textvariable
-tv_male = IntVar()
-tv_female = IntVar()
+
+
+
+
 
 
 
