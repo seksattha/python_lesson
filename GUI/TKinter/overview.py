54f9b8f6fc53d63a22c7cd510358d8b516a23c9c
seksatta_HP <seksattha@gmail.com>
2024-07-12 09:19:28 +0700

    3 เพิ่มการแสดง title

--- GUI/TKinter/overview.py
+++ GUI/TKinter/overview.py
@@ -1,8 +1,9 @@
 import tkinter as tk # import tkinter แล้วทำการตั้งชื่อว่า tk
 
 root = tk.Tk()             #คำว่า root จะเป้นอะไรก็ได้ main windows หรืออะไรก็ได้
-
-tk.Label(root, text="Hello World").grid()
+root.option_add("*Font" ,"consolars 20 ") # เป็นการตั้งค่าทั้งหมด
+root.title("Tkinter Example") # กำหนด title
+tk.Label(root, text="Hello World").grid() #
 root.mainloop()
 
 # พื้นฐานก็เขียนเท่านี้ก็พอ
