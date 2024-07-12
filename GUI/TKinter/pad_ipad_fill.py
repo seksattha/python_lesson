305d0fa0738156e49d54c93d1dd299a5483b9293
seksatta_HP <seksattha@gmail.com>
2024-07-12 09:43:08 +0700

    1 เริ่มด้วยการกำหนดตำแหน่งด้วย pack ก่อน

---
+++ GUI/TKinter/pad_ipad_fill.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+# fill , padx, pady
+
+root = Tk()
+Label(root, text="Mocha").pack(side= TOP)
+Label(root, text="Espresso").pack(side= TOP)
+Label(root, text="Latte").pack(side= TOP)
+root.mainloop()
